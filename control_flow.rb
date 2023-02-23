89f01ad41c1cc870b1f6e99588365b24eefa3b96
jumst.code@gmail.com <jumiyoon@Jumis-MacBook-Pro.local>
2023-02-23 13:35:20 -0800

    commit tree 61b1ab47c82f041d3a963a748a453ce8606f1a3d

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,49 @@
 def admin_login(username, password)
-  # your code here
+  if (username == "admin" || username == "ADMIN") && (password=="12345")
+    "Access granted"
+  else
+    "Access denied"
+  end
 end
 
 def hows_the_weather(temperature)
-  # your code here
+  if 40 > temperature 
+    "It's brisk out there!"
+  elsif 40 <= temperature && temperature <=65
+    "It's a little chilly out there!"
+  elsif 85 < temperature
+    "It's too dang hot out there!"
+  else
+    "It's perfect out there!"
+  end
 end
 
 def fizzbuzz(num)
-  # your code here
+  if (num%3 == 0 && num%5 != 0)
+    "Fizz"
+  elsif (num%3.0 != 0 && num%5.0 == 0)
+    "Buzz"
+  elsif (num%3.0 == 0 && num%5.0 == 0)
+    "FizzBuzz"
+  else
+    num
+  end 
 end
 
+
 def calculator(operation, num1, num2)
-  # your code here
+  case operation
+  when "+"
+    num1+num2
+  when "-"
+    num1-num2
+  when "*"
+    num1*num2
+  when "/"
+    num1/num2
+  else
+    puts "Invalid operation!"
+    nil
+  end
 end
 
